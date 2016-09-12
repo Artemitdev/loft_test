064af369ecbaa18610999766aff50cb78e3daa42
Artem Kravtsov <artemitdev@gmailcom>
2016-09-12 16:44:42 +0300

    Иконка часов first_scripts

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
+/*(function(){
+		var checkbox = document.querySelectorAll('input'),
+				block = document.querySelectorAll('.course_items');
 
-jQuery(document).ready(function() {
- $("input:checkbox").change(function(){
-	if ($("#checkbox-js").filter(":checked").length)
-		$(".course_items").addClass("submit");
-		$(".course_items__img, .desc, field_complete").addClass("bg_done");
-	});
+		var changeBackground = function(){
+			if(this.checked == true) {
+				block.classList.add('submit', 'bg_done');
+			}
+		};
+
+		for ( var i = 0, len = checkbox.lenght; i < len; i++){
+			checkbox[i].addEventListener('click', changeBackground, false);
+		};
+
+})();*/
+
+$(function() {
+  $(".course_items .field_complete input").click(function() {
+    $(".course_items .field_complete input").addClass('submit');
+    // var index = $(this).parent().index( "ul li" );
+  })
 });
+
+
 
 
