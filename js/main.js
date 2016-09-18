4b48a749976583637bdc729b6976afbae096aa3c
Artem Kravtsov <artemitdev@gmailcom>
2016-09-18 23:46:03 +0300

    Правки JQuery - переписал логику

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
+// $(function(){
+
+// 	var mainBlock = $(".course_items");
+// 	var blockLenght = $(".course_items").lenght;
+// 	var block = $(".field_complete");
+
+// 	$(mainBlock).click(function (e){
+// 		for (i = blockLenght; i > 0; i--){
+// 			if ($("input").is(':checked')){
+// 				$(mainBlock).addClass('submit bg_done').fadeOut(1000);
+// 			} else if (i = 0){
+// 					$(".courses").css("display", "none");
+// 					$(".done").css("display", "block");
+// 				};
+// 		};
+// 	});
+// });
+
+// $(function(){
+// 	$(".course_items").click(function (e){
+// 		var block = $(".course_items").length;
+// 		var i = block;
+// 		for (i; i > 0; i--){
+// 			if ($("input").is(':checked')){
+// 				$(this).addClass('submit bg_done').fadeOut(1000);
+// 			} else if (i = 0){
+// 					$(".courses").css("display", "none");
+// 					$(".done").css("display", "block");
+// 				};
+// 		};
+// 	});
+// });
+
 $(function(){
-	
-	$(".course_items").click(function (e){
-		var block = $(".course_items").length;
-		for (i = block; i > 0; i--){
-			if ($("input").is(':checked')){
-				$(this).addClass('submit bg_done').fadeOut(1000);
-			} else if (i = 0){
-					$(".courses").css("display", "none");
-					$(".done").css("display", "block");
-				};
-		};
+	$(".field_complete").click(function (event){
+		var $target = $(event.target);
+		if ($target.is("input")){
+			$target.closest(".course_items").addClass('submit bg_done').fadeOut(1000);
+		} else if ($(".course_items").css('display') !== 'block') {
+				$(".courses").css("display", "none");
+				$(".done").css("display", "block");
+			}
 	});
 });
 
