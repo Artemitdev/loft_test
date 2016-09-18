a18d5972c0af2dcdc70e2ecb9f91b67132f8a50f
Artem Kravtsov <artemitdev@gmailcom>
2016-09-19 01:40:38 +0300

    Правки JQuery - реализация через цикл

--- js/main.js
+++ js/main.js
@@ -1,48 +1,20 @@
-// $(function(){
-
-// 	var mainBlock = $(".course_items");
-// 	var blockLenght = $(".course_items").lenght;
-// 	var block = $(".field_complete");
-
-// 	$(mainBlock).click(function (e){
-// 		for (i = blockLenght; i > 0; i--){
-// 			if ($("input").is(':checked')){
-// 				$(mainBlock).addClass('submit bg_done').fadeOut(1000);
-// 			} else if (i = 0){
-// 					$(".courses").css("display", "none");
-// 					$(".done").css("display", "block");
-// 				};
-// 		};
-// 	});
-// });
-
-// $(function(){
-// 	$(".course_items").click(function (e){
-// 		var block = $(".course_items").length;
-// 		var i = block;
-// 		for (i; i > 0; i--){
-// 			if ($("input").is(':checked')){
-// 				$(this).addClass('submit bg_done').fadeOut(1000);
-// 			} else if (i = 0){
-// 					$(".courses").css("display", "none");
-// 					$(".done").css("display", "block");
-// 				};
-// 		};
-// 	});
-// });
 
 $(function(){
 	$(".field_complete").click(function (event){
 		var $target = $(event.target);
-		if ($target.is("input")){
-			$target.closest(".course_items").addClass('submit bg_done').fadeOut(1000);
-		} else if ($(".course_items").css('display') !== 'block') {
-				$(".courses").css("display", "none");
-				$(".done").css("display", "block");
-			}
+		var items = $(".course_items");
+		for (var i = items.length; i >= 0; i--) {
+			if ($target.is("input")){
+				$target.closest(".course_items").addClass('submit bg_done');
+				setTimeout(function(){
+					$(".course_items.submit").remove();
+					},1000);
+			};
+			// $(".courses").css("display", "none"),
+			// $(".done").css("display", "block");
+		};
 	});
 });
-
 
 
 
