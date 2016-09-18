78ae3d9c0a09775615144767d427ce76bd4dc18d
Artem Kravtsov <artemitdev@gmailcom>
2016-09-18 17:10:56 +0300

    Правки JQuery

--- js/main.js
+++ js/main.js
@@ -1,43 +1,18 @@
-/*(function(){
-		var checkbox = document.querySelectorAll('input'),
-				block = document.querySelectorAll('.course_items');
-
-		var changeBackground = function(){
-			if(this.checked == true) {
-				block.classList.add('submit', 'bg_done');
-			}
+$(function(){
+	
+	$(".course_items").click(function (e){
+		var block = $(".course_items").length;
+		for (i = block; i > 0; i--){
+			if ($("input").is(':checked')){
+				$(this).addClass('submit bg_done').fadeOut(1000);
+			} else if (i = 0){
+					$(".courses").css("display", "none");
+					$(".done").css("display", "block");
+				};
 		};
-
-		for ( var i = 0, len = checkbox.lenght; i < len; i++){
-			checkbox[i].addEventListener('click', changeBackground, false);
-		};
-
-})();*/
-
-$(function() {
-  $("input").click(function(event) {
-
-  	if ($(".course_items .field_complete input").is(':checked')){
-  		$(".course_items").addClass('submit bg_done');
-  		$(".done").css("display", "block");
-  		$(".courses").css("display", "none");
-
-    setTimeout(function(){
-    	$('.course_items').fadeOut('fast')
-    },2000);
-  	}
-  })
+	});
 });
 
-// $("input").change(function(){
-// 	this.checked&&($(".courses-item").removeClass("active"),
-// 		$(this).closest(".courses-item").addClass("active"),setTimeout(function(){
-// 			$(".courses-item.active").remove()
-// 		},1e3),
-// 		setTimeout(function(){$("*").hasClass("courses-item")||($(".courses-header").remove(),
-// 			$(".complete").css("display","block")
-// 			)},
-// 		1e3))});
 
 
 
